524a38714d8133b73a0450a64d248ed453296151
unknown <CSD5241@.CATTECHNOLOGIES.COM>
2010-09-18 11:49:12 +0530

    created models for client module

---
+++ app/models/client_contact.rb
@@ -0,0 +1,2 @@
+class ClientContact < ActiveRecord::Base
+end
---
+++ db/migrate/20100918053646_create_client_contacts.rb
@@ -0,0 +1,20 @@
+class CreateClientContacts < ActiveRecord::Migration
+  def self.up
+    create_table :client_contacts do |t|
+      t.references :clients
+      t.string :email, :limit => 100
+      t.string :home_phone 
+      t.string :mobile
+      t.string :first_name, :limit => 40
+      t.string :last_name, :limit => 40
+      t.string :user_name, :limit => 40
+      t.string :password, :limit => 40
+      t.boolean :send_login_info
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :client_contacts
+  end
+end
